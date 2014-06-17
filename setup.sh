1ede040119e2b06df0e3cac83f7c02159af2978e
Michael Spear <michael.spear@backslashinteractive.com>
2014-06-17 14:11:56 -0500

    install fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 if [ `whoami` != 'root' ] ; then
 	echo 'Please run as root'
 	exit
@@ -12,19 +11,19 @@ if [ `uname` == 'Linux' ] || [ `uname` == 'FreeBSD' ] ; then
 		echo 'You Must Have Ruby Installed to Proceed'
 		exit
 	fi
-	
-	#check if the curses gem is installed
+
+	if [ ! `gem which curses` ] ; then
+		echo 'please install the curses gem'
+		exit
+	fi
 	
 	mkdir -p /usr/local/bin/ruby_text_edit
 	cp *.rb /usr/local/bin/ruby_text_edit
-	
 	echo '#!/bin/sh'> /usr/local/bin/redit
 	echo 'ruby /usr/local/bin/ruby_text_edit/run.rb $*'>> /usr/local/bin/redit
 	chmod +x /usr/local/bin/redit
 	chmod +x /usr/local/bin/ruby_text_edit/*.rb
-	
 	echo 'install finished type $ redit to use'
-	
 else
 	echo 'Operating System Not Supported by Installer'
 fi
